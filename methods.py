360c5b60adba7a210033a90f0cde48df44468228
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2019-06-18 22:04:27 +0100

    Changed to units of cMpc/h, since its the input units for ICs

--- methods.py
+++ methods.py
@@ -4,14 +4,14 @@ class simulation():
 
     def __init__(self):
         self.h = 0.6777
-        self.boxsize = 3200
-        self.r = 10. / self.h
+        self.boxsize = 3200 * self.h
+        self.r = 14. 
         self.grid = 1200
         self.conv = (self.boxsize/self.grid)
 
     def show(self):
         print('Hubble parameter:',self.h)
-        print('Boxsize (cMpc):',self.boxsize)
+        print('Boxsize (cMpc/h):',self.boxsize)
         print('Grid dimensions:',self.grid,'^3')
 
 
