71cc32b7344e2e5475792eb912a397224d068af4
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2019-12-10 18:43:37 +0000

    Added labels and smaller spacing for fit input

--- fit.py
+++ fit.py
@@ -1,7 +1,7 @@
 """
 
     Script to load the delta values created using the chosen gridsize and plots
-    the gaussian fit to log10(1+delta). 
+    the gaussian fit to log(1+delta).
 
 """
 
@@ -14,7 +14,7 @@ from scipy.spatial.distance import cdist
 from methods import simulation, grid_coordinates
 
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
@@ -28,18 +28,19 @@ mu, sig = norm.fit(delta_log.flatten())
 
 norm = norm(loc=mu,scale=sig)
 
-import pickle as pcl
-pcl.dump(norm,open('log_delta_fit.p','wb'))
+#import pickle as pcl
+#pcl.dump(norm,open('log_delta_fit.p','wb'))
 
 ## Plot distribution and fit
 fig, ax = plt.subplots(1,1)
-ax.hist(delta_log.flatten(), density='normed',bins=100)
-x = np.linspace(-3,3,100)
-ax.plot(x,norm.pdf(x))
+ax.hist(delta_log.flatten(), density='True', bins=500)
+x = np.linspace(-2,2,500)
+ax.plot(x,norm.pdf(x), lw=1)
 ax.set_xlim(np.min(delta_log)-0.1,np.max(delta_log)+0.1)
 ax.grid(True)
 ax.text(0.2, 2.5, r'$\mu={}$'.format(np.round(mu,4)), fontsize = 14)
 ax.text(0.2, 2., r'$\sigma={}$'.format(np.round(sig,4)), fontsize = 14)
 ax.set_xlabel(r'ln(1+$\delta$)', fontsize = 15)
-plt.savefig('log_fit.png')
-# plt.show()
+ax.set_ylabel(r'PDF', fontsize = 15)
+plt.savefig('log_fit.pdf')
+plt.show()
