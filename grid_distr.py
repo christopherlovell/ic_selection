dc9a4716bfd97c1993412fb44e20fe58dc9ce94b
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2020-03-26 12:09:40 +0000

    changed number of bins to 50, new plotstyle, twinaxes

--- grid_distr.py
+++ grid_distr.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import gc
 from methods import simulation
-import matplotlib
+import matplotlib as mpl
+mpl.rcParams['text.usetex'] = True
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_context("paper")
@@ -41,11 +43,15 @@ def get_xyz(sim):
 
 sim = simulation()
 sim.show()
+nbins = 50
 
-# delta = np.load('conv_output.npz')['delta']
-# delta_log = np.log10(delta)
-# delta_sorted = np.sort(delta_log.flatten())
-bins = np.linspace(np.min(delta_log)-0.01, np.max(delta_log)+0.01, 26, endpoint = True)
+delta = np.load('conv_output.npz')['delta']
+delta_log = np.log10(delta)
+delta_sorted = np.sort(delta_log.flatten())
+bins = np.linspace(np.min(delta_log)-0.01, np.max(delta_log)+0.01, nbins+1, endpoint = True)
+
+mu, sig = norm.fit(delta_log.flatten())
+norm = norm(loc=mu,scale=sig)
 
 ## Read in the resimmed regions
 print_df = pd.read_csv('GEAGLE_regions.txt')
@@ -68,20 +74,43 @@ for jj in range(len(pos)):
 
     del_grids[jj] = delta_log[(xx,yy,zz)]
 
-fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize=(6, 6), sharex=True, sharey=True,
+
+fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize=(7, 6), sharex=True, sharey=True,
                             facecolor='w', edgecolor='k')
-ax.hist(delta_sorted, density='True', bins=500, label = 'All grids', color = 'black', ls = 'dashed', histtype = 'step', lw = 1.5, stacked = True)
-ax.hist(del_grids.flatten(), density='True', bins=bins, label = 'Resim grids', color = 'black', histtype = 'step', lw = 2, stacked = True)
+ax1 = ax.twinx()
+
+ax.hist(delta_log.flatten(), density='True', histtype = 'stepfilled', bins=150)
+n, nbins = np.histogram(del_grids.flatten(), bins=bins)
+x = np.linspace(-1,1,500)
+ax.plot(x,norm.pdf(x), lw=1.5, color='black', ls='dashed', label='All grids')
+ax.hist(del_grids.flatten(), density='True', bins=bins, label = 'Resim grids', color = 'black', histtype = 'step', lw = 1.5)
+ax1.hist(del_grids.flatten(), density='True', bins=bins, label = 'Resim grids', color = 'black', histtype = 'step', lw = 1.5, alpha = 0.)
+
+twinyticks = np.array([100, 1e3, 2e3, 3e3, 4e3, 5e3, 6e3, 7e3])
+twinyticklabels = ['100', '1000', '2000', '3000', '4000', '5000', '6000', '7000']
+yticks = twinyticks/(np.sum(n)*(bins[1]-bins[0]))
+
+ax.text(-0.27, 2.5, r'$\mu={}$'.format(np.round(mu,4)), fontsize = 13)
+ax.text(-0.27, 2., r'$\sigma={}$'.format(np.round(sig,4)), fontsize = 13)
 
 ax.set_xlim(np.min(delta_log)-0.02,np.max(delta_log)+0.02)
-ax.grid(True)
-ax.set_xlabel(r'log$_{10}$(1+$\delta$)', fontsize = 18)
-ax.set_ylabel(r'PDF', fontsize = 18)
+ax.grid(True, alpha = 0.4)
+ax.set_ylim(0., 8)
+ax1.set_ylim(0., 8)
+ax.set_xlabel(r'log$_{10}$(1+$\delta$)', fontsize = 13)
+ax.set_ylabel(r'PDF', fontsize = 13)
+
+ax1.set_ylabel(r'N', fontsize = 13)
+ax1.set_yticks(yticks)
+ax1.set_yticklabels(twinyticklabels, fontsize = 13)
+
 for label in (ax.get_xticklabels() + ax.get_yticklabels()):
-    label.set_fontsize(15)
-ax.text(0.1, 7.1, '- - All grids', color = 'black', fontsize = 15)
-ax.text(0.1, 6.8, '__ ', color = 'black', fontsize = 15)
-ax.text(0.14, 6.6, 'Resim grids', color = 'black', fontsize = 15)
-fig.tight_layout()
+    label.set_fontsize(12)
+
+ax.text(0.1, 7.1, '- - All grids', color = 'black', fontsize = 13)
+ax.text(0.1, 6.8, '---', color = 'black', fontsize = 13)
+ax.text(0.125, 6.7, 'Resim grids', color = 'black', fontsize = 13)
+
+
 fig.savefig('sampling_bins.pdf')
 plt.show()
